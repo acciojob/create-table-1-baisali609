c24c0f4007e9701785fd21f02da4f623adb68ac2
baisali609 <baisali609@gmail.com>
2025-02-14 09:32:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
+      let table = document.getElementById("sampleTable");
+
+	let row1 = table.insertRow(0);
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+    
+    // Set the text content of the new cells
+    cell1.textContent = "New Cell1";
+    cell2.textContent = "New Cell2";
   
 }
